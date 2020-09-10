64b93490fede0c16e26288208748dec97bd446b8
Prathima Achanta <prathimaachanta@prathimantasmbp.lan>
2020-09-09 22:07:31 -0700

    first branch commit

--- firstfile.java
+++ firstfile.java
@@ -3,3 +3,4 @@ This is the second line of the code
 This is the second line of code from the editor from Mac
 Third line from the mac
 Fourth line from mac
+This is the first commit from branch in MAC
